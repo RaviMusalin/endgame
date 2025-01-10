cb0c7dd6524a97f116261fe45ec7c8a441b05008
RaviMusalin <sheikhmusalin@gmail.com>
2025-01-09 21:25:27 -0500

    Completed win CSS

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@
 /**
  * Goal: Build out the main parts of our app
  * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
+ * Challenge: Build a status section below the header.
+ * For now, you can just hard-code in the styles for
+ * a winning game, and we'll make it more dynamic
+ * later.
  */
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <h1>Assembly Endgame:</h1>
         <p>Guess the word in under 8 attempts to keep the <br />programming world safe from Assembly!</p> 
       </header>
+      <div className="gameStatus">You Win!<br />Well Done</div>
     </div>
   );
 }
